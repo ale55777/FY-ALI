ff976ebe08013b9b1eb09b535deff0bd5e4cbb10
Ali Raza Khan <malirazakhan55777@gmail.com>
2026-04-13 14:38:30 +0500

    Update form error text color

--- frontend/src/components/common/FormError.tsx
+++ frontend/src/components/common/FormError.tsx
@@ -6,7 +6,7 @@ interface FormErrorProps {
 const FormError= ({ message }:FormErrorProps) => {
   if (!message) return null;
 
-  return <div className="text-red-600 w-full h-auto">{message}</div>;
+  return <div className="text-green-600 w-full h-auto">{message}</div>;
 };
 
 export default FormError;
